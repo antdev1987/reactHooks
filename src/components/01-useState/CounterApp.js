0e1ae98715400464c5489789ea281ea31272d0c7
antdev1987 <antdev1987@gmail.com>
2021-12-20 23:14:53 -0600

    105. useState

---
+++ src/components/01-useState/CounterApp.js
@@ -0,0 +1,36 @@
+import './counterApp.css'
+import { useState } from 'react'
+
+
+const CounterApp = () => {
+
+    const [counter, setCounter] = useState({
+        counter1:10,
+        counter2:20
+    })
+
+    const {counter1,counter2} =counter
+
+
+    return (
+        <>
+            <h1>Counter {counter1}</h1>
+            <h1>Counter {counter2}</h1>
+            <hr />
+
+            <button
+                className='btn btn-primary'
+                onClick={() => {
+                    setCounter({
+                        ...counter,
+                        counter1:counter1 + 1
+                    })
+                }}
+            >
+                +1
+            </button>
+        </>
+    )
+}
+
+export default CounterApp
